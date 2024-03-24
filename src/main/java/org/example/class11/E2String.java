ec6b76f34d17e88a811c1cc52332fdc48280cde0
sergeystrik <sergeystrik@gmail.com>
2024-03-24 11:17:45 -0400

    commit

---
+++ src/main/java/org/example/class11/E2String.java
@@ -0,0 +1,11 @@
+package org.example.class11;
+
+public class E2String {
+    public static void main(String[] args) {
+        StringBuilder st=new StringBuilder("Sunday");
+        st.append("Monday");
+        System.out.println(st);
+
+
+    }
+}
