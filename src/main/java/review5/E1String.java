b3584293a5f3d6985c22ba64b9d94803406e3853
sergeystrik <sergeystrik@gmail.com>
2024-03-30 09:57:22 -0400

    commit

---
+++ src/main/java/review5/E1String.java
@@ -0,0 +1,11 @@
+package review5;
+
+public class E1String {
+    public static void main(String[] args) {
+      //String str=new String("Java");
+      String str="Java";
+      int len =str.length();
+        System.out.println(len);
+
+    }
+}
