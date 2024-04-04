da517b7171ddc78bd234961db379f770d9f76029
sergeystrik <sergeystrik@gmail.com>
2024-04-03 20:15:18 -0400

    commit

---
+++ src/main/java/org/example/class16/Cat.java
@@ -0,0 +1,17 @@
+package org.example.class16;
+
+public class Cat {
+    String name;
+    String color;
+    int age;
+    double weight;
+    void speak(){
+        System.out.println("Meow");
+    }
+    void  sleep(){
+        System.out.println("ZZzzzzzz");
+    }
+    void printInfo(){
+        System.out.println(name+" "+color+" "+age+"" +age+" "+weight);
+    }
+}
---
+++ src/main/java/org/example/class16/Dog.java
@@ -0,0 +1,17 @@
+package org.example.class16;
+
+public class Dog {
+    String name;
+    String color;
+    int age;
+    double weight;
+    void bark(){
+        System.out.println("Wuff");
+    }
+    void sleep(){
+        System.out.println("ZZzzzzzz");
+    }
+    void printInfo(){
+        System.out.println(name+" "+color+" "+age+" "+weight);
+    }
+}
