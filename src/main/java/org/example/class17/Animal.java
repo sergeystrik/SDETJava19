021e8f7c4e3c370b44305c50996694207469f87a
sergeystrik <sergeystrik@gmail.com>
2024-04-07 10:22:29 -0400

    commit

--- src/main/java/org/example/class17/Animal.java
+++ src/main/java/org/example/class17/Animal.java
@@ -1,19 +1,47 @@
 package org.example.class17;
 
-public class Animal {
-   private String name;
-   private String color;
-   private  int age;
-   private double weight;
-   private Animal(String name,String color,int age,double weight){
-       this.name=name;
-       this.color=color;
-       this.age=age;
-       this.weight=weight;
-   }
-   void  printInfo(){
-       System.out.println(name+" "+color+" "+weight);
-   }
 
-}
+    public class Animal {
+        private String name;
+        private String color;
+        private int age;
+        private double weight;
+
+
+        public Animal(String name) {
+            this.name = name;
+        }
+
+        public Animal(String name, String color, int age, double weight) {
+            this.name = name;
+            this.color = color;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        void printInfo() {
+            System.out.println(name + " " + color + " " + age + " " + weight);
+        }
+
+    }
+
+    class Dog extends Animal {
+
+        Dog(String name, String color) {
+            super(name);
+        }
+
+        Dog(String name, String color, int age, double weight) {
+            super(name, color, age, weight);
+        }
+    }
+
+    class AnimalTester {
+        public void main(String[] args) {
+            Dog d = new Dog("jacky", "black", 10, 20);
+            d.printInfo();
+        }
+
+    }
+
 
