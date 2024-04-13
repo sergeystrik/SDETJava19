4a480b3b7cb277d434947a174f9d2cf69da42fdb
sergeystrik <sergeystrik@gmail.com>
2024-04-13 15:17:50 -0400

    commit

--- src/main/java/org/example/class20/PlayerDemo.java
+++ src/main/java/org/example/class20/PlayerDemo.java
@@ -1,7 +1,18 @@
 package org.example.class20;
 
-public class PlayerDemo {
-    public static void main(String[] args) {
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class PlayerDemo {
+    public static void main(String[] args) throws FileNotFoundException, JavaLayerException {
+
+        FileInputStream fits=new FileInputStream("C:\\Users\\ssiry\\IdeaProjects\\SDETJava19\\src\\main\\java\\org\\example\\class20\\Ambient-music-free.mp3");
+
+        Player p=new Player(fits);
+
+        p.play();
     }
 }
